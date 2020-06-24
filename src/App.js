680759c4daa5086fb59f867c69b730de96e1f0c2
Gregory Young <gregoryjryoung@gmail.com>
2020-06-24 17:18:59 -0400

    First Commit. Making ul list of names, usernames, and websites

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      items:[],
+      isLoaded: false,
+    }
+  }  
+  
+  increment = () => {
+
+  }
+  
+  componentDidMount(){
+    // put code here
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        console.log(res);
+        return res.json();
+      })
+      .then(data => {
+         console.log(data)
+        this.setState({
+          isLoaded: true,
+          items: data,
+        })
+      })
+  }
+  
+  
+  
+  render() {
+    let {isLoaded, items} = this.state;
+    return(
+      <div>
+        <div>
+          <ul>
+            <li>Name: Leanna Graham | UserName: Bret |<a href="http://www.google.com">Webite</a> </li>
+            <li>Name: Ervin Howell | UserName: Antonette |<a href="http://www.bing.com">Website</a></li>
+            <li>Name: Clementine Bauch | UserName: Samantha |<a href="http://www.yahoo.com">Website</a></li>
+            <li>Name: Patricia Lebsack | UserName: Karianne |<a href="http://www.askjeeves.com">Website</a></li>
+            <li>Name: Chelsey Dietrich | UserName: Kamren |<a href="http://www.google.com">Website</a> </li>
+            <li>Name: Mrs. Dennis Schulist | UserName: Leopoldo_Corkey |<a href="http://www.bing.com">Website</a></li>
+            <li>Name: Kurtis Weissnat| UserName: Elqyn.Skiles | <a href="http://www.yahoo.com">Website</a></li>
+            <li>Name: Nicholas Runolfsdottir| UserName: Maxime_Nienow |<a href="http://www.askjeeves.com">Website</a></li>
+            <li>Name: Glenna Reichert| UserName: Delphine |<a href="http://www.google.com">Webite</a> </li>
+            <li>Name: Clementina DuBuque| UserName: Moriah.Stanton |<a href="http://www.bing.com">Website</a></li>
+          </ul>
+        </div>
+      </div>
+    )
+  };
+};
 
 export default App;
